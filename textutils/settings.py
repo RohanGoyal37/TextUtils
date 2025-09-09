6349baa8bdf3c3dd465283a4aa9b6b57b80887a3
Rohan Goyal <rohangoyal264@gmail.com>
2025-09-09 09:11:18 +0000

    Fix template directory path in settings to use BASE_DIR

--- textutils/settings.py
+++ textutils/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'textutils.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'], # This is the directory where all the html files are stored
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
