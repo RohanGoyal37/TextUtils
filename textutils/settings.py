a7b32c54796969e2c4c046029d6a5e20a8e04f5e
Rohan Goyal <rohangoyal264@gmail.com>
2025-09-09 07:41:10 +0000

    Update settings to allow specific host and refactor analyze function for improved readability and efficiency

--- textutils/settings.py
+++ textutils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q^+$ccgu%73e92i2(tk4)*g--2%m1wu3*t0e#!359^djrzkk$0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['37rohan.pythonanywhere.com']
 
 
 # Application definition
