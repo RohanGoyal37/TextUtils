3ba1314b244fab3e9adcb6bde06ab455e1c0322f
Rohan Goyal <rohangoyal264@gmail.com>
2025-09-09 08:53:48 +0000

    update

--- textutils/settings.py
+++ textutils/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q^+$ccgu%73e92i2(tk4)*g--2%m1wu3*t0e#!359^djrzkk$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['37rohan.pythonanywhere.com']
 
